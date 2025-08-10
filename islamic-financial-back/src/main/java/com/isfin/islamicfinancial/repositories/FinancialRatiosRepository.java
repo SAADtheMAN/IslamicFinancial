2795bd04a71d36c33db53fb1eb7ef14c0170fc04
SAADtheMAN <162479154+SAADtheMAN@users.noreply.github.com>
2025-08-10 20:58:28 +0200

    Delete islamic-financial-back directory

--- islamic-financial-back/src/main/java/com/isfin/islamicfinancial/repositories/FinancialRatiosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isfin.islamicfinancial.repositories;
-
-import com.isfin.islamicfinancial.entities.FinancialRatios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialRatiosRepository extends JpaRepository<FinancialRatios, Long> {
-}
--- islamic-financial-back/src/main/java/com/isfin/islamicfinancial/repositories/StockScreenerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isfin.islamicfinancial.repositories;
-
-import com.isfin.islamicfinancial.entities.StockScreener;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StockScreenerRepository extends JpaRepository<StockScreener, Long> {
-    List<StockScreener> findByShariahCompliant(Boolean shariahCompliant);
-}
